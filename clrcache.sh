63fcb25ec8ec56f8c849303e7ed866c3cf728bfd
lim169 <ksfuldrew@gmail.com>
2017-03-06 18:22:01 +0100

    question start

--- clrcache.sh
+++ clrcache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-php bin/console assetic:dump  && php bin/console cache:clear && rm -rf var/cache/dev && rm -rf var/cache/prod
+gulp sass && php bin/console assetic:dump  && php bin/console cache:clear && rm -rf var/cache/dev && rm -rf var/cache/prod
--- src/App/CourseBundle/Controller/CourseController.php
+++ src/App/CourseBundle/Controller/CourseController.php
@@ -6,11 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class CourseController extends Controller
 {
-    public function courseAction()
+    public function indexAction()
     {
         return $this->render('AppCourseBundle:Course:index.html.twig', array(
             // ...
         ));
+    }
+
+    public function questionAction(Request $request)
+    {
+        return $this->render('CourseBundle:Course:question.html.twig', $form_data);
     }
 
 }
